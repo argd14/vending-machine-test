5ce8ad33a8bb79ffcbb847eef44ff157c9363f69
argd14 <argd14@gmail.com>
2021-09-02 14:07:39 -0600

    fetched products from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const url = 'https://vending-machine-test.vercel.app/api/products';
+  const [state, setstate] = useState()
+const fetchApi = async ()=>{
+  const res = await fetch(url)
+  console.log(res.status);
+  const resJSON = await res.json()
+  setstate(resJSON)
+  console.log(resJSON);
+}
+
+useEffect(()=>{
+  fetchApi()
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola</h1>
     </div>
   );
 }
